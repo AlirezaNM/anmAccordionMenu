ebc2f927add4e1e15d99f6408d5472cb54b4e6d0
Alireza <anm8010@gmail.com>
2018-08-19 17:49:39 +0430

    added js folder

---
+++ js/anmAccordion.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+
+    $.fn.anmAccordion = function (userOptions) {
+
+        var options = $.extend({}, userOptions)
+
+        this.each(function () {
+
+            var elm = $(this);
+
+            elm.click(function () {
+                var myContent = $(this).next(".accContent");
+                $(".accContent").slideUp();
+                if (myContent.css("display") == "none")
+                    myContent.slideDown();
+
+            })
+
+        })
+
+    }
+
+})(jQuery)
